3edbadacce753ad034fa97cbfa53b284a5eaef38
Nils Martin Sande <nils.martin.sande@nav.no>
2026-03-30 18:12:39 +0200

    Oppdaterte nes keybinds

--- dot-config/nvim/lua/plugins/copilot.lua
+++ dot-config/nvim/lua/plugins/copilot.lua
@@ -9,10 +9,23 @@ require("copilot").setup({
 	},
 	nes = {
 		enabled = true,
+		-- NES keymaps registered by the plugin are normal-mode only.
+		-- Insert-mode bindings are added separately in keymaps.lua.
 		keymap = {
-			accept_and_goto = "<C-a>",
+			accept_and_goto = "<M-a>",
 			accept = false,
-			dismiss = "<C-x>",
+			dismiss = "<M-e>",
 		},
 	},
 })
+
+-- Insert-mode NES keymaps (plugin only registers normal-mode ones)
+local nes_api = require("copilot.nes.api")
+vim.keymap.set("i", "<M-a>", function()
+	if nes_api.nes_apply_pending_nes() then
+		nes_api.nes_walk_cursor_end_edit()
+	end
+end, { desc = "NES: accept suggestion", silent = true })
+vim.keymap.set("i", "<M-e>", function()
+	nes_api.nes_clear()
+end, { desc = "NES: dismiss suggestion", silent = true })
